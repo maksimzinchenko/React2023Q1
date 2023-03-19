282efd7fe6fccdd84b78cbb48df5653863e568c1
lipkij <lipkij@gmail.com>
2023-03-19 23:48:37 +0400

    fix: fix css rules for container (Sun, Mar 19, 2023 11:48:34 PM)

--- react-components/src/components/PageTitle.tsx
+++ react-components/src/components/PageTitle.tsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Container from '../UI/Container';
 import Navbar from './Navbar';
 
 type Props = {
@@ -9,8 +10,10 @@ class PageTitle extends Component<Props, never> {
   render() {
     return (
       <header>
-        <h1>{this.props.title}</h1>
-        <Navbar />
+        <Container>
+          <h1>{this.props.title}</h1>
+          <Navbar />
+        </Container>
       </header>
     );
   }
